e03e1907a78c856089e20db64ed90777d670fc13
cookTanxin <newtanwenfu@gmail.com>
2024-02-22 16:51:45 +0800

    change

--- 06/react-learning/src/store/reducer.js
+++ 06/react-learning/src/store/reducer.js
@@ -1,14 +1,20 @@
 // 导入模块 counter 和 area
 import {areaReducer} from './area'
 import {counterReducer} from './counter'
+// 使用combincerReducer 合并多个reducer 模块
+import {combineReducers} from 'redux'
 
 
 
+// function reducer(state = {},action) {
+//     return {
+//         counterInfo:counterReducer(state.counterInfo,action),
+//         areaInfo: areaReducer(state.areaInfo,action)
+//     }
+// }
 
-function reducer(state = {},action) {
-    return {
-        counterInfo:counterReducer(state.counterInfo,action),
-        areaInfo: areaReducer(state.areaInfo,action)
-    }
-}
+const reducer = combineReducers({
+    counterInfo:counterReducer,
+    areaInfo:areaReducer
+})
 export default reducer
